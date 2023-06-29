8197946a73bfa894f65d8a51dae8d628ffd8aedc
MuP0claB <miroslav.ivanoff@gmail.com>
2023-06-29 18:20:32 +0300

    Added routing to PhonesPage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import AboutPage from "./pages/AboutPage";
 import RegistrationPage from "./pages/RegistrationPage";
+import PhonesPage from "./pages/PhonesPage";
 
 function App() {
   return (
     <main>
       <Routes>
-        <Route path="/" element={<Navigate replace to='welcome'/>} />
-        <Route path="/welcome/*" element={<p>WELCOME TO MY PAGE</p>}/>
+        <Route path="/" element={<Navigate replace to="welcome" />} />
+        <Route path="/welcome/*" element={<p>WELCOME TO MY PAGE</p>} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/phones" element={<PhonesPage />} />
       </Routes>
     </main>
   );
