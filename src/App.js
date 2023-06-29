213dcb788dfac46114fca8bae7714e99ff146018
MuP0claB <miroslav.ivanoff@gmail.com>
2023-06-30 02:39:20 +0300

    Added styling to the RegistrationForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import "./App.css";
+
 import AboutPage from "./pages/AboutPage";
 import RegistrationPage from "./pages/RegistrationPage";
 import PhonesPage from "./pages/PhonesPage";
--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,27 +1,30 @@
+import styles from "./RegistrationForm.module.css";
+
 const RegistrationForm = () => {
   return (
     <section>
       <form>
         <div>
           <label>First Name</label>
-          <input type="text"/>
+          <input type="text" />
         </div>
         <div>
           <label>Last Name</label>
-          <input type="text"/>
+          <input type="text" />
         </div>
         <div>
           <label>E-mail Address</label>
-          <input type="email"/>
+          <input type="email" />
         </div>
         <div>
           <label>Password</label>
-          <input type="password"/>
+          <input type="password" />
+        </div>
+        <div>
+          <button className={styles.btn_register}>Register</button>
+          <button className={styles.btn_back}>Back To Home</button>
         </div>
       </form>
-      <div>
-        <button>Register</button>
-      </div>
     </section>
   );
 };
