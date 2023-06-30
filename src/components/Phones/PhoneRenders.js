db70fbb5f3e072c33277a513fee2caaf89c52230
MuP0claB <miroslav.ivanoff@gmail.com>
2023-06-30 18:02:18 +0300

    Added some styling and modified phone rendering

--- src/components/Phones/PhoneRenders.js
+++ src/components/Phones/PhoneRenders.js
@@ -1,13 +1,45 @@
-import styles from './PhoneRenders.module.css';
+import styles from "./PhoneRenders.module.css";
 
 const PhoneRenders = () => {
-    return <section>
-        <ul>
-            <li>Google Pixel 7a</li>
-            <li>Apple iPhone 14 Pro</li>
-            <li>OnePlus 11 Pro</li>
-        </ul>
+  return (
+    <section>
+      <ul>
+        <li>
+          <div>
+            <img
+              className={styles.thumbnail}
+              src={
+                "https://m-cdn.phonearena.com/images/phones/83956-350/Google-Pixel-7a.webp"
+              }
+            />
+            <p>Google Pixel 7a</p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img
+              className={styles.thumbnail}
+              src={
+                "https://m-cdn.phonearena.com/images/phones/83629-350/Apple-iPhone-14-Pro.webp"
+              }
+            />
+            <p>Apple iPhone 14 Pro</p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <img
+              className={styles.thumbnail}
+              src={
+                "https://m-cdn.phonearena.com/images/phones/83799-350/OnePlus-11.webp"
+              }
+            />
+            <p>OnePlus 11</p>
+          </div>
+        </li>
+      </ul>
     </section>
+  );
 };
 
 export default PhoneRenders;
